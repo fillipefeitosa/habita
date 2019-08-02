996b4f6e3fca11451cf24be13e1d53697fcfbc2d
Fillipe Feitosa <fillfeitosa@gmail.com>
2019-08-02 10:42:23 +0100

    First commit with arquitecture and profile functionality

---
+++ imports/startup/client/routes.js
@@ -0,0 +1,50 @@
+import { FlowRouter } from 'meteor/kadira:flow-router';
+import { BlazeLayout } from 'meteor/kadira:blaze-layout';
+import { Session } from 'meteor/session';
+
+// Import needed templates
+import '../../ui/layouts/body/body.js';
+import '../../ui/pages/home/home.js';
+import '../../ui/pages/not-found/not-found.js';
+import '../../ui/pages/login/login.js';
+
+FlowRouter.notFound = {
+  action() {
+    BlazeLayout.render('App_body', { main: 'App_notFound' });
+  },
+};
+
+// Exposed group
+exposed = FlowRouter.group({});
+exposed.route('/login', {
+    name: 'login',
+    action: function(){
+        BlazeLayout.render('login', { top: "header", main: "App_home", pageContent:"info"});
+    }
+});
+
+// Internal App routes
+// Logged Routes
+loggedIn = FlowRouter.group({
+    prefix: '/',
+    triggersEnter: [
+        function(){
+            var route;
+            if(!(Meteor.loggingIn() || Meteor.userId())){
+                route = FlowRouter.current();
+                if (route.route.name !== 'login') {
+                    // Adicionar Mensagem de Redirecionamento
+                    Session.set('redirectAfterLogin',route.path);
+                }
+                return FlowRouter.go('login');
+            }
+        }
+    ]
+});
+
+loggedIn.route("/", {
+    name: 'App.home',
+    action() {
+      BlazeLayout.render('App_body', { main: 'App_home' });
+    },
+});
---
+++ imports/ui/components/profile/profile.js
@@ -0,0 +1,81 @@
+import './profile.html';
+
+Template.profile.helpers({
+    'userProfile': function(){
+        const userProfile = Meteor.user().profile;
+        return userProfile;
+    },
+    states(){
+        stateArray = [
+            ["Açores"],
+            ["Aldeia Rica"],
+            ["Baraçal"],
+            ["Cortegada"],
+            ["Cadafaz"],
+            ["Soutomoninho"],
+            ["Carrapichana"],
+            ["Cortiçô da Serra"],
+            ["Mourela"],
+            ["Porteira"],
+            ["Casas do Rio"],
+            ["Celorico Gare"],
+            ["Forno Telheiro"],
+            ["Quinta do Salgueiro"],
+            ["Lajeosa do Mondego"],
+            ["Assanhas"],
+            ["Linhares da Beira"],
+            ["Quintãs de Cima"],
+            ["Maçal do Chão"],
+            ["Carvalheda"],
+            ["Mesquitela"],
+            ["Mogadouro"],
+            ["Vale da Ribeira"],
+            ["Minhocal"],
+            ["Prados"],
+            ["Rapa"],
+            ["Ratoeira"],
+            ["Salgueirais"],
+            ["Celorico da Beira"],
+            ["Espinheiro"],
+            ["Lameiras"],
+            ["Aldeia da Serra"],
+            ["Casas do Soeiro"],
+            ["Fonte Arcada"],
+            ["Grichoso"],
+            ["Mourilhe"],
+            ["Soutinho"],
+            ["Velosa"],
+            ["Galisteu"],
+            ["Vide entre Vinhas"],
+            ["Vila Boa do Mondego"],
+            ["Termas de Santo António"],
+            ["Quintãs de Baixo"],
+        ]
+        return stateArray.sort();
+    },
+
+
+});
+
+Template.profile.events({
+    "keyup [name=name]": function(event){
+        const name = $(event.target).val();
+        Meteor.users.update({_id: Meteor.userId()}, {$set: {"profile.name": name}});
+    },
+    "keyup [name=institution]": function(event){
+        const institution = $(event.target).val();
+        Meteor.users.update({_id: Meteor.userId()}, {$set: {"profile.institution": institution}});
+    },
+    "keyup [name=profession]": function(event){
+        const profession = $(event.target).val();
+        Meteor.users.update({_id: Meteor.userId()}, {$set: {"profile.profession": profession}});
+    },
+    "change [name=headquarters]": function(event){
+        const headquarters = $(event.target).val();
+        Meteor.users.update({_id: Meteor.userId()}, {$set: {"profile.headquarters": headquarters}});
+    },
+    "change [name=sites]": function(event){
+        const sites = $(event.target).val();
+        Meteor.users.update({_id: Meteor.userId()}, {$set: {"profile.sites": sites}});
+    },
+});
