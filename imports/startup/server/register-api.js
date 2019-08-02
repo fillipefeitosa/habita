c4f46bafcaac4eb38b16bd69bef7fff38fc73662
Fillipe Feitosa <fillfeitosa@gmail.com>
2019-08-02 17:39:43 +0100

    Removed test api

--- imports/startup/server/register-api.js
+++ imports/startup/server/register-api.js
@@ -3,10 +3,6 @@
 import '../../api/links/methods.js';
 import '../../api/links/server/publications.js';
 
-// Books api
-import '../../api/books/books.js';
-import '../../api/books/server/publications.js';
-
 // Consults api
 import '../../api/consults/consults.js';
 import '../../api/consults/server/publications.js';
